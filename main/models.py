5c012835e882ff31f6a1d112d23e469b0a30223d
satkar <satkarphuyal@hotmail.com>
2021-02-14 12:39:23 +0545

    fixing all

--- main/models.py
+++ main/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class File(models.Model):
+    name = models.CharField(max_length=255, blank=True, null=True )
+    url = models.CharField(max_length=500,  blank=True, null=True)
+
+    def __str__(self):
+        return self.url
--- main/tasks.py
+++ main/tasks.py
@@ -2,6 +2,7 @@ from celery import shared_task
 from celery_progress.backend import ProgressRecorder
 from .utils import scraper_spectra
 import csv
+import boto3
 import time
 from time import sleep
 
@@ -27,13 +28,13 @@ from time import sleep
 #         print("heheheheh")
 #     return 'Done'
 
-
+from .models import File
 
 
 @shared_task(bind=True)
 def go_to_sleep(self, duration):
     progress_recorder = ProgressRecorder(self)
-    with open("nepal.csv", 'w') as myfile:
+    with open("ecat.csv", 'w') as myfile:
         wr = csv.writer(myfile, quoting=csv.QUOTE_ALL)
         # wr.writerow(["Input Part #","Output Part#","Part Name","OE"])
         total = len(duration)
@@ -41,5 +42,16 @@ def go_to_sleep(self, duration):
             a = scraper_spectra(row[0])
             progress_recorder.set_progress(i+1, total, row[0])
             wr.writerow(a[0])
+
+        a= File.objects.all().count()+1
+        filename="spectrapremium"+a
+        s3 = boto3.resource('s3')
+        bucketname = "scrapers1"
+        folder = "spectrapremium"
+        s3.Bucket(bucketname).put_object(ContentType= "application/CSV", ACL='public-read',
+                                         Key='{0}/{1}'.format(folder, filename), Body=myfile)
+        url = "https://{0}.s3.amazonaws.com/{1}/{2}".format(bucketname, folder, filename)
+        print(url)
+        File.objects.create(name="spectrapremium",url=url)
 
     return 'Done'
