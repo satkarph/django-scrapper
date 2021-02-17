5371e63dbff75e7455ce01cae0d30653946616c3
satkar <satkarphuyal@hotmail.com>
2021-02-17 11:03:54 +0545

    add migration for creadted date

--- main/utils2.py
+++ main/utils2.py
@@ -3,7 +3,7 @@ import pandas as pd
 import boto3
 from .models import File
 def store_s3(filecsv,folder,filename):
-    df = pd.read_csv(filecsv, sep='delimiter', header=None)
+    df = pd.read_csv(filecsv, sep='delimiter')
     with io.BytesIO() as output:
         with pd.ExcelWriter(output, engine='xlsxwriter') as writer:
             df.to_excel(writer,index=False)
