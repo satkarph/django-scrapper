958f70e867bc9d72c29f7d609859bdce3ebf5b14
satkar <satkarphuyal@hotmail.com>
2021-02-18 22:00:47 +0545

    fixed all app

--- main/utils2.py
+++ main/utils2.py
@@ -3,13 +3,15 @@ import pandas as pd
 import boto3
 from .models import File
 def store_s3(filecsv,folder,filename):
-    df = pd.read_csv(filecsv, sep='delimiter')
+    df = pd.read_csv(filecsv, sep=',',header=None)
     with io.BytesIO() as output:
         with pd.ExcelWriter(output, engine='xlsxwriter') as writer:
             df.to_excel(writer,index=False)
         data = output.getvalue()
 
     bucketname = "scrapers1"
+    # bucketname = "iatcrm"
+
 
     s3 = boto3.resource('s3')
     s3.Bucket(bucketname).put_object(Key='{0}/{1}'.format(folder, filename), Body=data, ACL='public-read')
