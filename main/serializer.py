9c7a69ec325e7a935d5393705da83b60fc8a71e2
satkar <satkarphuyal@hotmail.com>
2021-02-25 19:56:35 +0545

    fixing

--- main/serializer.py
+++ main/serializer.py
@@ -1,6 +1,8 @@
 from rest_framework import serializers
 from .models import File
 from datetime import datetime
+import pytz
+
 class FileSer(serializers.ModelSerializer):
     time = serializers.SerializerMethodField()
 
@@ -16,6 +18,8 @@ class FileSer(serializers.ModelSerializer):
 
     def get_time(self, obj):
         date = obj.created_date
+        est = pytz.timezone('US/Eastern')
+        date =date.astimezone(est)
         d = date.strftime("%Y-%m-%d %H:%M:%S")
         d = datetime.strptime(d, "%Y-%m-%d %H:%M:%S").strftime("%Y-%m-%d %I:%M:%S %p")
         return d
